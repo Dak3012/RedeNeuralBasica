71d0014fba22bf982cbb7021c1ee881b566a826e
Danilo de Arruda Kuhlmann <danilo.kuhlmann@hotmail.com>
2022-01-16 19:38:45 -0300

    Adição de arquivos de testes

--- RedeNeural/CamadaNeural.cs
+++ RedeNeural/CamadaNeural.cs
@@ -3,22 +3,22 @@
 /// </summary>
 public class CamadaNeural
 {
-	public double[,] ListaArestas { get; set; }
-	public CamadaNeural(double[,] listaArestas)
-	{
-		ListaArestas = listaArestas;
-	}
-	public CamadaNeural(int NOrigem, int NDestino)
-	{
-		ListaArestas = new double[NOrigem,NDestino];
-		for (int EndereçoOrigem = 0; EndereçoOrigem < NOrigem; EndereçoOrigem++)
-		{
-			for (int EndereçoDestino = 0; EndereçoDestino < NDestino; EndereçoDestino++)
-			{
-				ListaArestas[EndereçoOrigem, EndereçoDestino] = new Random().NextDouble();
-			}
-		}
-	}
+    public double[,] ListaArestas { get; set; }
+    public CamadaNeural(double[,] listaArestas)
+    {
+        ListaArestas = listaArestas;
+    }
+    public CamadaNeural(int NOrigem, int NDestino)
+    {
+        ListaArestas = new double[NOrigem, NDestino];
+        for (int EndereçoOrigem = 0; EndereçoOrigem < NOrigem; EndereçoOrigem++)
+        {
+            for (int EndereçoDestino = 0; EndereçoDestino < NDestino; EndereçoDestino++)
+            {
+                ListaArestas[EndereçoOrigem, EndereçoDestino] = new Random().NextDouble();
+            }
+        }
+    }
 }
 /// <summary>
 /// Nova rede neural, inicia uma novo "individuo" com as camadas randomicas.
@@ -29,61 +29,64 @@ public class CamadaNeural
 /// <param name="QuantidadeCamadas">Número de camadas intermeiaras da rede neuras</param>
 public class RedeNeural
 {
-	public List<CamadaNeural> CamadaNeural { get; set; }
-	public RedeNeural(int Entradas, int Saidas, int tamanhoCamadas, int QuantidadeCamadas)
-	{
-		CamadaNeural = new List<CamadaNeural>();
-		CamadaNeural.Add(new CamadaNeural(Entradas, tamanhoCamadas)); //Camada neural de entrada
-		for (int nCamada = 0; nCamada < QuantidadeCamadas; nCamada++)
-		{
-			CamadaNeural.Add(new CamadaNeural(tamanhoCamadas, tamanhoCamadas)); //Camadas intermediarias
-		}
-		CamadaNeural.Add(new CamadaNeural(tamanhoCamadas, Saidas)); //Camada Neural de Saida
-	}
-	/// <summary>
-	/// Deep Clone
-	/// </summary>
-	/// <returns></returns>
-	/// <exception cref="Exception"></exception>
-	public RedeNeural CloneRede()
-	{
-		var objectserialize = JsonConvert.SerializeObject(this).ToString();
-		var newObject = JsonConvert.DeserializeObject<RedeNeural>(objectserialize);
-		if (newObject == null)
-		{
-			throw new Exception("Objeto inválido");
-		}
-		else
-		{
-			return newObject;
-		}
-	}
-	/// <summary>
-	/// Randomiza a rede de acordo o dado de entrada como sendo o valor de 0 a 1, o grau de diferença entre os individuos.
-	/// </summary>
-	/// <param name="Diferença">Diferença Maxima entre as redes neurais</param>
-	/// <param name="grauMaximo">Grau maxímo da diferença entre os mesmos pares de arestas</param>
-	/// <returns></returns>
-	public RedeNeural RandomStats(double Diferença, double grauMaximo)
-	{
-		var redeNerural = this.CloneRede();
-		var camadasNeurais = redeNerural.CamadaNeural;
-		var random = new Random();
-		foreach (var CamadaNeural in camadasNeurais)
-		{
-			var arestas = CamadaNeural.ListaArestas;
-			var dimensionX = arestas.GetUpperBound(1);
-			var dimensionY = arestas.GetUpperBound(2);
-			for(var x=0; x < dimensionX; x++)
-			{
-				for(var y=0;y< dimensionY; y++)
-				{
-					var dou = random.NextDouble();
-					if()
-					arestas[x,y] = 
-				}
-			}
-		}
-		return redeNerural;
-	}
+    public List<CamadaNeural> CamadaNeural { get; set; }
+    public RedeNeural(int Entradas, int Saidas, int tamanhoCamadas, int QuantidadeCamadas)
+    {
+        CamadaNeural = new List<CamadaNeural>();
+        CamadaNeural.Add(new CamadaNeural(Entradas, tamanhoCamadas)); //Camada neural de entrada
+        for (int nCamada = 0; nCamada < QuantidadeCamadas; nCamada++)
+        {
+            CamadaNeural.Add(new CamadaNeural(tamanhoCamadas, tamanhoCamadas)); //Camadas intermediarias
+        }
+        CamadaNeural.Add(new CamadaNeural(tamanhoCamadas, Saidas)); //Camada Neural de Saida
+    }
+    /// <summary>
+    /// Deep Clone
+    /// </summary>
+    /// <returns></returns>
+    /// <exception cref="Exception"></exception>
+    public RedeNeural CloneRede()
+    {
+        var objectserialize = JsonConvert.SerializeObject(this).ToString();
+        var newObject = JsonConvert.DeserializeObject<RedeNeural>(objectserialize);
+        if (newObject == null)
+        {
+            throw new Exception("Objeto inválido");
+        }
+        else
+        {
+            return newObject;
+        }
+    }
+    /// <summary>
+    /// Randomiza a rede de acordo o dado de entrada como sendo o valor de 0 a 1, o grau de diferença entre os individuos.
+    /// </summary>
+    /// <param name="Diferença">Diferença Maxima entre as redes neurais</param>
+    /// <param name="grauMaximo">Grau maxímo da diferença entre os mesmos pares de arestas</param>
+    /// <returns></returns>
+    public RedeNeural RandomStats(double Diferença, double grauMaximo)
+    {
+        var redeNerural = this.CloneRede();
+        var camadasNeurais = redeNerural.CamadaNeural;
+        var random = new Random();
+        foreach (var CamadaNeural in camadasNeurais)
+        {
+            var arestas = CamadaNeural.ListaArestas;
+            var dimensionX = arestas.GetUpperBound(1);
+            var dimensionY = arestas.GetUpperBound(2);
+            for (var x = 0; x < dimensionX; x++)
+            {
+                for (var y = 0; y < dimensionY; y++)
+                {
+                    var dou = random.NextDouble();
+                    if (dou > Diferença)
+                    {
+                        var valor = random.NextDouble() * grauMaximo * 2 - 1 + arestas[x,y];
+                        arestas[x, y] = valor > 1?1:(valor < -1?-1:valor);
+                    }
+                }
+            }
+        }
+        return redeNerural;
+    }
 }
---
+++ RedeNeuralBasicaTests/RedeNeuralTests.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Tests
+{
+    [TestClass()]
+    public class RedeNeuralTests
+    {
+        [TestMethod()]
+        public void RandomStatsTest()
+        {
+            Assert.Fail();
+        }
+
+        [TestMethod()]
+        public void RedeNeuralTest()
+        {
+            Assert.Fail();
+        }
+
+        [TestMethod()]
+        public void CloneRedeTest()
+        {
+            Assert.Fail();
+        }
+
+        [TestMethod()]
+        public void RandomStatsTest1()
+        {
+            Assert.Fail();
+        }
+    }
+}
